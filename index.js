964994ae881ace03dc34f4468bd66f9ecd9feb2b
Erin Lyden <irishjedi77@mac.com>
2020-06-05 12:39:28 -0700

    added variables

---
+++ index.js
@@ -0,0 +1,35 @@
+//Week 1 Javascript - Section 4
+
+let newCar = 25000;
+let usedCar = 13000;
+
+let erinWallet = 42;
+let steveWallet = 100;
+
+let friends = 374;
+let newFriends = 6;
+
+let myAge = 42;
+let petAge = 8;
+
+let firstName1 = "Erin";
+let firstName2 = "Steve";
+
+let lastName1 = "Lyden";
+let lastName2 = "Schulke";
+
+let middleInitial1 = "J";
+let middleInitial2 = "T";
+
+//Week 1 Javascript - Section 5 and Section 6
+
+let newWallet = erinWallet - newCar;
+console.log("The amount of money in my wallet is " + newWallet);
+
+let friendsAnnually = friends / myAge; 
+console.log("The number of friends I've made annually in my lifetime is " + friendsAnnually);
+
+let fullName = firstName1 + " " + middleInitial1 + ". " + lastName1;
+console.log("My full name is " + fullName);
+
+
